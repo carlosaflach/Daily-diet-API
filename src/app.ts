58b51d6f5490e88e511f4ac5dcab8fbf0d0484b7
Carlos Afonso <carlos.flach15@gmail.com>
2024-04-29 03:19:25 +0200

    feat: add cookie plugin

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import fastify from "fastify";
+import cookie from '@fastify/cookie'
 
 export const app = fastify();
+
+app.register(cookie);
 
 app.get('/hello', (request, response) => {
    response.status(200).send({ message: 'Hello from the server'})
