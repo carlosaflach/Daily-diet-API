0217247601153d40a23788327162aa797874ec5d
Carlos Afonso <carlos.flach15@gmail.com>
2024-04-29 23:05:13 +0200

    feat: add test to update a meal

--- tests/meals.test.ts
+++ tests/meals.test.ts
@@ -137,4 +137,40 @@ describe('Meals route', () => {
       .set('Cookie', userResponse.get('Set-Cookie') as string[])
       .expect(204)
   })
+
+  it('should be able to update a meal', async () => {
+    const userResponse = await request(app.server)
+      .post('/users')
+      .send({ name: 'Test User', email: 'user@test.com' })
+      .expect(201)
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', userResponse.get('Set-Cookie') as string[])
+      .send({
+        name: 'Test Meal 1',
+        description: 'This is the test meal 1',
+        isOnDiet: true,
+        date: new Date(),
+      })
+      .expect(201)
+
+    const mealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', userResponse.get('Set-Cookie') as string[])
+      .expect(200)
+
+    const mealId = mealsResponse.body.meals[0].id
+
+    await request(app.server)
+      .put(`/meals/${mealId}`)
+      .set('Cookie', userResponse.get('Set-Cookie') as string[])
+      .send({
+        name: 'Test Meal 2',
+        description: 'This is the test meal 2',
+        isOnDiet: true,
+        date: new Date(),
+      })
+      .expect(204)
+  })
 })
