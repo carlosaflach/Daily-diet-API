fd0aacd5ebce6869d4b4a35838390981799fad92
Carlos Afonso <carlos.flach15@gmail.com>
2024-04-29 22:23:27 +0200

    feat: add test case to list all meals

--- tests/meals.test.ts
+++ tests/meals.test.ts
@@ -34,4 +34,42 @@ describe('Meals route', () => {
       })
       .expect(201)
   })
+
+  it('should be able to list all meals from a user', async () => {
+    const userResponse = await request(app.server)
+      .post('/users')
+      .send({ name: 'Test User', email: 'user@test.com' })
+      .expect(201)
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', userResponse.get('Set-Cookie') as string[])
+      .send({
+        name: 'Test Meal 1',
+        description: 'This is the test meal 1',
+        isOnDiet: true,
+        date: new Date(),
+      })
+      .expect(201)
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', userResponse.get('Set-Cookie') as string[])
+      .send({
+        name: 'Test Meal 2',
+        description: 'This is the test meal 2',
+        isOnDiet: true,
+        date: new Date(),
+      })
+      .expect(201)
+
+    const mealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', userResponse.get('Set-Cookie') as string[])
+      .expect(200)
+
+    expect(mealsResponse.body.meals).toHaveLength(2)
+    expect(mealsResponse.body.meals[0].name).toBe('Test Meal 1')
+    expect(mealsResponse.body.meals[1].name).toBe('Test Meal 2')
+  })
 })
