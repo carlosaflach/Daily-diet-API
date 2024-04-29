519efc98f24f0d6031d21eb30e788d5f3589aa82
Carlos Afonso <carlos.flach15@gmail.com>
2024-04-29 19:27:56 +0200

    feat: add way to validate session_id

---
+++ src/middlewares/validateSessionId.ts
@@ -0,0 +1,14 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export const checkSessionIdExists = async (
+  request: FastifyRequest,
+  response: FastifyReply,
+) => {
+  const { sessionId } = request.cookies
+
+  if (!sessionId) {
+    return response.status(401).send({
+      error: 'Unauthorized',
+    })
+  }
+}
